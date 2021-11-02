50d86e74d06d807855e18a9ea08bffbb1e5dd184
Jean-Christophe Gueriaud <jean-christophe@vaadin.com>
2021-11-02 16:37:08 +0200

    Bump the Vaadin version to 14.7.1

---
+++ custom-theme/src/main/java/org/vaadin/spring/tutorial/Dependency.java
@@ -0,0 +1,10 @@
+package org.vaadin.spring.tutorial;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+@NpmPackage(value = "@fortawesome/fontawesome-free", version = "5.15.1")
+//@CssImport(value = "./css/styles.css")
+public class Dependency {
+
+}
--- ui-bootstrap/src/main/java/org/vaadin/spring/tutorial/MainView.java
+++ ui-bootstrap/src/main/java/org/vaadin/spring/tutorial/MainView.java
@@ -15,56 +15,22 @@
  */
 package org.vaadin.spring.tutorial;
 
-import java.util.Locale;
-
-import com.vaadin.flow.component.dependency.CssImport;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.dom.Style;
-import com.vaadin.flow.i18n.LocaleChangeEvent;
-import com.vaadin.flow.i18n.LocaleChangeObserver;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.Theme;
 
-@CssImport("global.css")
-@CssImport("module.css")
+@Theme(themeFolder = "reusable-theme")
+//@CssImport("global.css")
+//@CssImport("module.css")
 @Route
-public class MainView extends VerticalLayout implements LocaleChangeObserver {
+public class MainView extends VerticalLayout {
 
-    private RouterLink link;
-
-    public MainView(@Autowired Greeter greeter,
-            @Autowired ExampleTemplate template) {
+    public MainView() {
         H1 heading = new H1("Vaadin + Spring examples");
-        
-        Label greeting = new Label(greeter.sayHello());
-        Style grretingStyle = greeting.getElement().getStyle();
-        grretingStyle.set("display", "block");
-        grretingStyle.set("margin-bottom", "10px");
 
-        Button button = new Button("Switch language to Chinese",
-                event -> getUI().get().setLocale(Locale.CHINESE));
-
-        link = new RouterLink(
-                getTranslation("root.navigate_to_component"),
-                ViewComponent.class);
-
-        Style linkStyle = link.getElement().getStyle();
-        linkStyle.set("display", "block");
-        linkStyle.set("margin-bottom", "10px");
-
-        add(heading, greeting, button, link, template);
-    }
-
-    @Override
-    public void localeChange(LocaleChangeEvent event) {
-        link.setText(
-                getTranslation("root.navigate_to_component"));
+        add(heading);
     }
 
 }
